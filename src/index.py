3efff019f1cc9e3dee3677c0dc725e4aba4af34f
Sergei <s.vasilchenko@busfor.com>
2020-02-22 13:13:16 +0300

    add changes for architecture

---
+++ src/index.py
@@ -0,0 +1,13 @@
+class Figure:
+
+    def input_name(self):
+        name = input()
+
+    def print_area(self):
+        pass
+
+    def print_angles(self):
+        pass
+
+    def print_perimeter(self):
+        pass
